bc5d620d956c1ffc1af3284c27c8e2162ed95dcf
Dianne Dietrich <dd388@cornell.edu>
2017-05-26 16:55:59 -0400

    Draft python code for md5sums; will need revision

--- htprep/md5zip.py
+++ htprep/md5zip.py
@@ -1,14 +1,20 @@
 #/usr/bin/env python3
 
 # Rewriting MD5 script for Python
+# Part 1; generating checksums
 
 import glob
 import argparse
 import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-b', '--barcodes-folder', metavar='[barcodes_folder]',
+parser.add_argument('-b', '--barcodes_folder', metavar='[barcodes_folder]',
                     required=False, default='.',
                     help='folder with barcodes folders')
 args = parser.parse_args()
+
+
+ignore = ['.DS_Store', 'Thumbs.db']
+
+
 
