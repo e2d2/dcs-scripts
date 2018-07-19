e614d7bca975d392cf6ea3f192af390770999b59
Dianne Dietrich <dd388@cornell.edu>
2018-07-19 13:55:30 -0400

    change first find param

--- bag-dirs/remove_hidden.sh
+++ bag-dirs/remove_hidden.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-find . -name ".DS_STORE" -exec rm {} \;
-find . -name "._.*" -exec rm {} \;
-find . -name "Thumbs.db" -exec rm {} \;
+find $1 -name ".DS_STORE" -exec rm {} \;
+find $1 -name "._.*" -exec rm {} \;
+find $1 -name "Thumbs.db" -exec rm {} \;
