2ea3da1c7a738edaa45b5fd9a6ea1c6d7f603209
Dianne Dietrich <dd388@cornell.edu>
2018-07-25 09:56:56 -0400

    -r flag for zip instead

--- bag-dirs/bag_dirs.sh
+++ bag-dirs/bag_dirs.sh
@@ -10,6 +10,6 @@ if [ -d "$1" ]; then
 	bagdir=${1%/}
 	for b in $(find ${bagdir} -type d -maxdepth 1 -mindepth 1); do
 		${BAGITCMD} ${b};
-		zip ${b}.zip ${b}/*; done
+		zip ${b}.zip -r ${b}/; done
 	else echo "Did not specify a target directory."
 fi
