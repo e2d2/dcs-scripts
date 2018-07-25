ca755608eca0012c0b4ff4bb0b3b2391afb0daac
Dianne Dietrich <dd388@cornell.edu>
2018-07-25 09:46:54 -0400

    QUOTING and error message

--- bag-dirs/bag_dirs.sh
+++ bag-dirs/bag_dirs.sh
@@ -6,9 +6,10 @@ BAGIT="python3 -m bagit"
 # Bagit command
 BAGITCMD="${BAGIT} --md5"
 
-if [ -d $1 ]; then
+if [ -d "$1" ]; then
 	bagdir=${1%/}
 	for b in $(find ${bagdir} -type d -maxdepth 1 -mindepth 1); do
 		${BAGITCMD} ${b};
 		zip ${b}.zip ${b}; done
+	else echo "Did not specify a target directory."
 fi
