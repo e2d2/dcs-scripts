466741e4478fa3d9b118bf024ba1ad135228d725
Dianne Dietrich <dd388@cornell.edu>
2018-07-25 13:26:57 -0400

    zip-bags.sh zips separately

--- bag-dirs/zip-bags.sh
+++ bag-dirs/zip-bags.sh
@@ -1,16 +1,15 @@
-#/bin/bash
+#!/bin/bash
 
-# Bagit executable
-BAGIT="python3 -m bagit"
-
-# Bagit command
-BAGITCMD="${BAGIT} --md5"
+CWD=$(pwd)
 
 if [ -d "$1" ]; then
 	bagdir=${1%/}
 	for b in $(find ${bagdir} -type d -maxdepth 1 -mindepth 1); do
-		${BAGITCMD} ${b};
-#		zip ${b}.zip -r ${b}/;
+		cd ${bagdir};
+		zipname=$(basename ${b});
+		zip ${zipname}.zip -r ${zipname};
+		cd ${CWD};
 	done
 	else echo "Did not specify a target directory."
 fi
+	
