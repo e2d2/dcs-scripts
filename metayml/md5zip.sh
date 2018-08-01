268a636c47929a15eb6246c331af2a5991759fe3
Dianne Dietrich <dd388@cornell.edu>
2018-08-01 09:07:07 -0400

    Slight adjustment with zip

--- metayml/md5zip.sh
+++ metayml/md5zip.sh
@@ -2,22 +2,27 @@
 
 set -o noclobber
 
+CWD=$(pwd);
 sdir=$(basename $1);
+
 if [[ -d ${sdir} ]]; then
     # First loop through and do checksum
     for sd in ${sdir}/*; do
         cd ${sd};
         md5sum * > checksum.md5;
-        cd -;
+        cd ${CWD};
     done
 
     # Second zip up things
     for sd in ${sdir}/*; do
+	cd ${sdir}
         basesd=$(basename ${sd});
-        zip ${basesd}.zip ${sdir}/${basesd}/*
+        zip ${basesd}.zip -r ${basesd}/*
+	cd ${CWD};
     done
 
     
 fi
 
 # TODO: Error message if first param isn't a directory
+
